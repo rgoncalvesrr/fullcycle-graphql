9f8bfc1fcf72dd50b91055dafa7fef61ded9e77c
Ricardo Gonçalves <ricardo@conceitho.com>
2024-06-01 18:36:21 -0300

    feat: adiciona criação/listagem de categorias

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -13,7 +13,18 @@ import (
 
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
-	panic(fmt.Errorf("not implemented: CreateCategory - createCategory"))
+
+	category, err := r.CategoryDB.Create(input.Name, *input.Description)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Category{
+		ID:          category.ID,
+		Name:        category.Name,
+		Description: &category.Description,
+	}, nil
 }
 
 // CreateCourse is the resolver for the createCourse field.
@@ -23,7 +34,22 @@ func (r *mutationResolver) CreateCourse(ctx context.Context, input model.NewCour
 
 // Categories is the resolver for the categories field.
 func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, error) {
-	panic(fmt.Errorf("not implemented: Categories - categories"))
+	categories, err := r.CategoryDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var result []*model.Category
+
+	for _, category := range categories {
+		result = append(result, &model.Category{
+			ID:          category.ID,
+			Name:        category.Name,
+			Description: &category.Description,
+		})
+	}
+
+	return result, nil
 }
 
 // Courses is the resolver for the courses field.
